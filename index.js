98a18aace037237d6caaf98952cf74547efb6bb9
Bunlong <bunlong.van@gmail.com>
2018-03-10 15:42:12 +0700

    upload to bitbucket

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-#!/usr/bin/env node --harmony
+#!/usr/bin/env node
+var fs = require('fs');
+var ProgressBar = require('progress');
 
+var chalk = require('chalk');
+var request = require('superagent');
 var co = require('co');
 var prompt = require('co-prompt');
 var program = require('commander');
@@ -12,9 +16,41 @@ program
     co(function *() {
       var username = yield prompt('username: ');
       var password = yield prompt.password('password: ');
-      console.log('user: %s pass: %s file: %s',
-        username, password, file
-      );
+      
+      var fileSize = fs.statSync(file).size;
+      var fileStream = fs.createReadStream(file);
+      var barOpts = {
+        width: 20,
+        total: fileSize,
+        clear: true
+      };
+      var bar = new ProgressBar(' uploading [:bar] :percent :etas', barOpts);
+      fileStream.on('data', function (chunk) {
+        bar.tick(chunk.length);
+      });
+
+      request
+        .post('https://api.bitbucket.org/2.0/snippets/')
+        .auth(username, password)
+        .attach('file', fileStream)
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+          if (!err && res.ok) {
+            var link = res.body.links.html.href;
+            console.log(chalk.bold.cyan('Snippet created: ') + link);
+            process.exit(0);
+          }
+          var errorMessage;
+          if (res && res.status === 401) {
+            errorMessage = "Authentication failed! Bad username/password?";
+          } else if (err) {
+            errorMessage = err;
+          } else {
+            errorMessage = res.text;
+          }
+          console.error(chalk.red(errorMessage));
+          process.exit(1);
+        });
     });
   })
   .parse(process.argv);
